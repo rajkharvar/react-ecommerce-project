6bc5eca67821ed060f5927d1247aca07a6c8e69a
Raj Kharvar <rajkharvar1808@gmail.com>
2019-12-02 20:34:16 +0530

    input component fix

--- src/components/form-input/FormInput.jsx
+++ src/components/form-input/FormInput.jsx
@@ -6,7 +6,15 @@ export const FormInput = ({ handleChange, label, ...otherProps }) => {
   return (
     <div className='group'>
       <input className='form-input' onChange={handleChange} {...otherProps} />
-      {label ? <label className='form-input-label'>{label}</label> : null}
+      {label ? (
+        <label
+          className={`${
+            otherProps.value.length ? 'shrink' : ''
+          } form-input-label`}
+        >
+          {label}
+        </label>
+      ) : null}
     </div>
   );
 };
