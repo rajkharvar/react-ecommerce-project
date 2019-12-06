125d50a8fbbdd01294498bfa1fd43fa87d2c2275
Raj Kharvar <rajkharvar1808@gmail.com>
2019-12-06 22:14:18 +0530

    yet another commit

--- src/components/cart-icon/CartIcon.jsx
+++ src/components/cart-icon/CartIcon.jsx
@@ -5,6 +5,7 @@ import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 import './cart-icon.scss';
 import { toggleHidden } from '../../redux/cart/cartAction';
 import { selectCartItemsCount } from '../../redux/cart/cartSelectors';
+import { createStructuredSelector } from 'reselect';
 
 const CartIcon = ({ itemCount, toggleHidden }) => {
   return (
@@ -15,8 +16,8 @@ const CartIcon = ({ itemCount, toggleHidden }) => {
   );
 };
 
-const mapStateToProps = state => ({
-  itemCount: selectCartItemsCount(state)
+const mapStateToProps = createStructuredSelector({
+  itemCount: selectCartItemsCount
 });
 
 const mapDispatchToProps = dispatch => ({
