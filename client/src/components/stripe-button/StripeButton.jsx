e6d595a58fa9a1fbc248899f74fa12b8c64148dd
Raj Kharvar <rajkharvar1808@gmail.com>
2019-12-17 17:42:56 +0530

    server init need to fix 404

--- client/src/components/stripe-button/StripeButton.jsx
+++ client/src/components/stripe-button/StripeButton.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
 
 export const StripeButton = ({ price }) => {
   const onToken = token => {
-    alert('Payment successful');
+    axios({
+      url: 'payment',
+      method: 'post',
+      data: {
+        amount: price * 100,
+        token: token
+      }
+    })
+      .then(response => {
+        alert('succesful payment');
+      })
+      .catch(error => {
+        console.log('Payment Error: ', error);
+        alert(
+          'There was an issue with your payment! Please make sure you use the provided credit card.'
+        );
+      });
   };
   return (
     <div>
